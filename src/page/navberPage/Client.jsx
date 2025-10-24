30dd1aa2be591c0a7e9ee74e63c9e8c2ddb951cc
Md Jihadul Islam <zihadvai42@gmail.com>
2025-10-24 23:05:16 +0600

    more

--- src/page/navberPage/Client.jsx
+++ src/page/navberPage/Client.jsx
@@ -1,107 +1,97 @@
 import React from "react";
+import Slider from "react-slick";
 import Container from "../../component/Container";
 import { FaStar } from "react-icons/fa";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Client = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4, // একসাথে কয়টা দেখাবে
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 640,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
+
+  const clients = [
+    {
+      name: "Md Jihadul Islam",
+      position: "CEO, Carbd",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
+    },
+    {
+      name: "Md Jihadul Islam",
+      position: "CEO, Carbd",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
+    },
+    {
+      name: "Md Jihadul Islam",
+      position: "CEO, Carbd",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
+    },
+    {
+      name: "Md Jihadul Islam",
+      position: "CEO, Carbd",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
+    },
+    {
+      name: "Md Jihadul Islam",
+      position: "CEO, Carbd",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
+    },
+  ];
+
   return (
-    <>
-      <Container>
-        <div className="flex gap-1 mt-2 mb-2 w-[100%] justify-center">
-          <div className="w-[25%] h-auto bg-gray-200 rounded-lg p-5 flex gap-5 items-center text-center px-10 my-20">
-            <div>
-              <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
-                <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-              </div>
+    <Container>
+      <div className="my-16">
+        <Slider {...settings}>
+          {clients.map((client, index) => (
+            <div key={index} className="px-3">
+              <div className="bg-gray-200 rounded-lg p-5 text-center relative my-10">
+                {/* Profile circle */}
+                <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
+                  <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+                </div>
 
-              <h3>Md Jihadul Islam</h3>
-              <p>CEO, Carbd</p>
-              <div className="flex text-yellow-400 gap-2 ctext-center justify-center my-2">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-              </div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Quibusdam quam, quos excepturi sed placeat vero labore, delectus
-                consequuntur ducimus voluptates non quidem? Tempora, itaque
-                blanditiis.
-              </p>
-            </div>
-          </div>
-          <div className="w-[25%] h-auto bg-gray-200 rounded-lg p-5 flex gap-5 items-center text-center px-10 my-20">
-            <div>
-              <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
-                <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-              </div>
+                {/* Info */}
+                <h3 className="font-semibold">{client.name}</h3>
+                <p className="text-sm text-gray-500">{client.position}</p>
 
-              <h3>Md Jihadul Islam</h3>
-              <p>CEO, Carbd</p>
-              <div className="flex text-yellow-400 gap-2 ctext-center justify-center my-2">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-              </div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Quibusdam quam, quos excepturi sed placeat vero labore, delectus
-                consequuntur ducimus voluptates non quidem? Tempora, itaque
-                blanditiis.
-              </p>
-            </div>
-          </div>
-          <div className="w-[25%] h-auto bg-gray-200 rounded-lg p-5 flex gap-5 items-center text-center px-10 my-20">
-            <div>
-              <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
-                <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-              </div>
+                {/* Stars */}
+                <div className="flex text-yellow-400 gap-2 justify-center my-2">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
 
-              <h3>Md Jihadul Islam</h3>
-              <p>CEO, Carbd</p>
-              <div className="flex text-yellow-400 gap-2 ctext-center justify-center my-2">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
+                {/* Review */}
+                <p className="text-gray-600 text-sm">{client.review}</p>
               </div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Quibusdam quam, quos excepturi sed placeat vero labore, delectus
-                consequuntur ducimus voluptates non quidem? Tempora, itaque
-                blanditiis.
-              </p>
             </div>
-          </div>
-          <div className="w-[25%] h-auto bg-gray-200 rounded-lg p-5 flex gap-5 items-center text-center px-10 my-20">
-            <div>
-              <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
-                <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-              </div>
-
-              <h3>Md Jihadul Islam</h3>
-              <p>CEO, Carbd</p>
-              <div className="flex text-yellow-400 gap-2 ctext-center justify-center my-2">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-              </div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Quibusdam quam, quos excepturi sed placeat vero labore, delectus
-                consequuntur ducimus voluptates non quidem? Tempora, itaque
-                blanditiis.
-              </p>
-            </div>
-          </div>
-        </div>
-      </Container>
-    </>
+          ))}
+        </Slider>
+      </div>
+    </Container>
   );
 };
 
--- src/page/navberPage/WorldPress.jsx
+++ src/page/navberPage/WorldPress.jsx
@@ -1,98 +1,25 @@
-import React from "react";
-import Slider from "react-slick";
+import React, { useState } from "react";
 import Container from "../../component/Container";
-import { FaStar } from "react-icons/fa";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Rank from "../../component/Rank";
+import ContentVideo from "../../component/ContentVideo";
+import ContentLogo from "../../component/ContentLogo";
+import Better from "./Better";
+import Client from "./Client";
 
-const Client = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 600,
-    slidesToShow: 4, // একসাথে কয়টা দেখাবে
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 2 },
-      },
-      {
-        breakpoint: 640,
-        settings: { slidesToShow: 1 },
-      },
-    ],
-  };
+const WorldPress = () => {
 
-  const clients = [
-    {
-      name: "Md Jihadul Islam",
-      position: "CEO, Carbd",
-      review:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
-    },
-    {
-      name: "Md Jihadul Islam",
-      position: "CEO, Carbd",
-      review:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
-    },
-    {
-      name: "Md Jihadul Islam",
-      position: "CEO, Carbd",
-      review:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
-    },
-    {
-      name: "Md Jihadul Islam",
-      position: "CEO, Carbd",
-      review:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
-    },
-    {
-      name: "Md Jihadul Islam",
-      position: "CEO, Carbd",
-      review:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quam, quos excepturi sed placeat vero labore.",
-    },
-  ];
 
   return (
-    <Container>
-      <div className="my-16">
-        <Slider {...settings}>
-          {clients.map((client, index) => (
-            <div key={index} className="px-3">
-              <div className="bg-gray-200 rounded-lg p-5 text-center relative my-10">
-                {/* Profile circle */}
-                <div className="w-[60px] h-[60px] bg-white rounded-full m-auto relative mt-[-45px] mb-2">
-                  <div className="w-[50px] h-[50px] bg-red-700 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-                </div>
-
-                {/* Info */}
-                <h3 className="font-semibold">{client.name}</h3>
-                <p className="text-sm text-gray-500">{client.position}</p>
-
-                {/* Stars */}
-                <div className="flex text-yellow-400 gap-2 justify-center my-2">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-
-                {/* Review */}
-                <p className="text-gray-600 text-sm">{client.review}</p>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
-    </Container>
+    <div className="mt-[100px]">
+      <Container>
+        <Rank />
+        <ContentLogo />
+        <ContentVideo />
+        <Better/>
+        <Client/>
+      </Container>
+    </div>
   );
 };
 
-export default Client;
+export default WorldPress;
