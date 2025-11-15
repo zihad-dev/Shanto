244c3cd5dd336a182c0c29989c94b960383c7869
Md Jihadul Islam <zihadvai42@gmail.com>
2025-11-15 19:46:16 +0600

    more

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -9,29 +9,31 @@ const Home = () => {
           {/* Left Section */}
           <div className="lg:w-1/2">
             <h1 className="max-w-full lg:max-w-[575px] font-['Work Sans'] font-semibold text-5xl sm:text-6xl md:text-7xl leading-[1.1] text-[#132238] mb-6">
-              Hello, I’m Brooklyn Gilbert
+              Hello, I’m Md Hasibur Rahman Shanto
             </h1>
             <p className="max-w-full lg:max-w-[648px] font-['Work Sans'] text-base sm:text-lg leading-7 text-[#556070] mb-6">
-              I'm a Freelance{" "}
+              {" "}
               <span className="relative cursor-pointer text-[#132238] after:absolute after:left-0 after:bottom-0 after:w-full after:h-2 after:bg-[#FFC8C8] after:-z-10">
-                UI/UX Designer
+                SEO
               </span>{" "}
-              and{" "}
+              &{" "}
               <span className="relative cursor-pointer text-[#132238] after:absolute after:left-0 after:bottom-0 after:w-full after:h-2 after:bg-[#FFC8C8] after:-z-10">
-                Developer
+                AI-Optimization Expert
               </span>{" "}
-              based in London, England. I strive to build immersive and beautiful web applications through carefully crafted code and user-centric design.
+              I help businesses rank higher, build trust, and convert visibility
+              into long-term growth through AEO, GEO, SXO & Local SEO.
             </p>
             <button className="font-['Work Sans'] font-semibold text-base sm:text-lg text-white py-3 px-6 bg-[#A53DFF] rounded-lg hover:bg-[#922ED1] transition-colors duration-300">
               Say Hello!
             </button>
 
             {/* Stats */}
-            <div className="mt-12 flex flex-wrap gap-4 sm:gap-6">
+            <div className="mt-10 flex gap-5">
               {[
-                { number: "15 Y.", label: "Experience" },
-                { number: "157+", label: "Project Completed" },
-                { number: "58", label: "Happy Client" },
+                { number: "3Y.", label: "Experience" },
+                { number: "37+", label: "Experience" },
+                { number: "23+", label: "Project Completed" },
+                { number: "35", label: "Happy Client" },
               ].map((stat, idx) => (
                 <div
                   key={idx}
@@ -50,7 +52,11 @@ const Home = () => {
 
           {/* Right Section */}
           <div className="lg:w-1/2 flex justify-center">
-            <div className="w-full max-w-[535px] h-[400px] sm:h-[500px] lg:h-[590px] bg-red-500 rounded-[15px]"></div>
+            <div className="max-w-[535px] h-[400px] sm:h-[500px] lg:h-[590px]  rounded-[15px]">
+              <div className="w-full h-full overflow-hidden rounded-[15px]">
+                <img src="images/Hasib Rahman.jpg" alt="Hasib Rahman" />
+              </div>
+            </div>
           </div>
         </div>
       </Container>
